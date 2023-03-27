e2748e15f53c26253094b675148ff6fc37b10f91
Camille LANGER <camille.langer@student.junia.com>
2023-03-27 17:17:30 +0200

    classes + boutton retour

--- calculatrice.js
+++ calculatrice.js
@@ -1,23 +1,45 @@
 let contenu=document.getElementById("calcul");
 
-function afficher(value){
-    console.log(value);
-    contenu.innerText+=value;
+class BaseCalculator {
+    constructor(contenu) {
+        this.contenu=contenu;
+        this.reversion=[]; //tableau de retour en arrière
+    }
+
+    afficher(value){
+        console.log(value);
+        this.contenu.innerText+=value;
+        this.reversion.push(this.contenu.innerText);
+    }
+
+    cancel(){
+        this.contenu.innerText= " ";
+    }
+
+    submit(){
+        let val=contenu.innerText;
+        let result= eval(val);
+        let historique=document.getElementById("historique");
+
+        let Div=document.createElement("div");//crée un element qui est une div
+
+        Div.innerText=val+"="+result;
+
+
+        historique.append(Div); // ajoute en fin
+        this.contenu.innerText=result;
+        this.reversion.push(this.contenu.innerText);
+    }
+
+    back(){
+        this.reversion.pop() //enlève le dernier element du tableau
+        if(this.reversion.length===0){ //on regarde si la liste est vide
+            this.contenu.innerText="";
+        }
+        else {
+            this.contenu.innerText=this.reversion[this.reversion.length-1];
+        }
+    }
 }
 
-function cancel(){
-    contenu.innerText= " ";
-}
-function submit(){
-    let val=contenu.innerText;
-    let result= eval(val);
-    let historique=document.getElementById("historique");
-
-    let Div=document.createElement("div");//crée un element qui est une div
-
-    Div.innerText=val+"="+result;
-
-
-    historique.append(Div); // ajoute en fin 
-    contenu.innerText=result;
-}
+let calculator = new BaseCalculator(contenu);
