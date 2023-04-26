77483174c82946a347647efb14e009d26d530bc8
Camille LANGER <camille.langer@student.junia.com>
2023-04-26 17:07:17 +0200

    calculatrice fini avec le serveur

--- calculatrice.js
+++ calculatrice.js
@@ -4,6 +4,13 @@ class BaseCalculator {
     constructor(contenu) {
         this.contenu=contenu;
         this.reversion=[]; //tableau de retour en arrière
+
+        this.url = 'http://localhost:3000';
+        this.xhttp = new XMLHttpRequest();
+        this.xhttp.onload = function() {
+            console.log(this.responseText);
+        }
+        this.date=Date.now();
     }
 
     afficher(value){
@@ -17,18 +24,37 @@ class BaseCalculator {
     }
 
     submit(){
-        let val=contenu.innerText;
-        let result= eval(val);
-        let historique=document.getElementById("historique");
-
-        let Div=document.createElement("div");//crée un element qui est une div
-
-        Div.innerText=val+"="+result;
+        try {
+            let val=contenu.innerText;
+            let result= eval(val);
 
 
-        historique.append(Div); // ajoute en fin
-        this.contenu.innerText=result;
-        this.reversion.push(this.contenu.innerText);
+            let historique=document.getElementById("historique");
+
+            let Div=document.createElement("div");//crée un element qui est une div
+
+            Div.innerText=val+"="+result;
+
+
+            historique.append(Div); // ajoute en fin
+            this.contenu.innerText=result;
+            this.reversion.push(this.contenu.innerText);
+
+            this.xhttp.open("POST",'http://localhost:3000/success-event', true);
+            this.xhttp.setRequestHeader("Content-type", "application/json");
+            this.xhttp.send(JSON.stringify({
+                timeTakenMs:Math.abs(new Date()-this.date)
+            }));
+            this.date=Date.now();
+
+        }catch (error){
+            this.xhttp.open("POST", 'http://localhost:3000/error-event', true);
+            this.xhttp.setRequestHeader("Content-type", "application/json");
+            this.xhttp.send(JSON.stringify({
+                created_at:Date.now()
+            }));
+        }
+
     }
 
     back(){
@@ -43,3 +69,8 @@ class BaseCalculator {
 }
 
 let calculator = new BaseCalculator(contenu);
+
+
+
+
+
